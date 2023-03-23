0043ef06bf4efc9891e79c26cc7f14420891d351
Gabriel Moreira <gsm.br2003@gmail.com>
2023-03-23 16:57:38 -0300

    faet: add loading more data when scroll list

--- src/screens/PlantSelect.tsx
+++ src/screens/PlantSelect.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
 import { useEffect, useState } from "react"
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native"
+import { ActivityIndicator, FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native"
 
 import { Button } from "../components/Button"
 import { EnvironmentButton } from "../components/EnvironmentButton"
@@ -39,6 +39,10 @@ export const PlantSelect = () => {
     const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>()
     const [loading, setLoading] = useState(true)
 
+    const [page, setPage] = useState(1)
+    const [loadingMore, setLoadingMore] = useState(false)
+    const [loadedAll, setLoadedAll] = useState(false)
+
     function handleEnvironmentSelected(environment: string) {
         setEnvironmentSelected(environment)
 
@@ -50,7 +54,7 @@ export const PlantSelect = () => {
     }
 
     async function fetchEnvironment() {
-        const {data} = await api.get('plants_environment?_sort=title&_order=asc')
+        const {data} = await api.get(`plants_environment?_sort=title&_order=asc`)
         setEnvironments([
             {
                 key: 'all',
@@ -60,13 +64,30 @@ export const PlantSelect = () => {
         ])
     }
 
-    async function fetchPlants() {
-        setLoading(true)
+    function handleFetchMore(distance: number) {
+        if(distance < 1) return 
 
-        const {data} = await api.get('plants?_sort=name&_order=asc')
-        setPlants(data)
-        setFilteredPlants(data)
+        setLoadingMore(true)
+        setPage(oldValue => oldValue + 1)
+
+        fetchPlants()
+    }
+
+    async function fetchPlants() {
+        const {data} = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=6`)
+
+        if(!data) return setLoadedAll(true)
+
+        if(page > 1) {
+            setPlants(oldValue => [...oldValue, ...data])
+            setFilteredPlants(oldValue => [...oldValue, ...data])
+        } else {
+            setPlants(data)
+            setFilteredPlants(data)
+        }
+
         setLoading(false)
+        setLoadingMore(false)
     }
 
     useEffect(() => {
@@ -115,6 +136,11 @@ export const PlantSelect = () => {
                 showsVerticalScrollIndicator={false}
                 numColumns={2}
                 contentContainerStyle={styles.plantList}
+                onEndReachedThreshold={0.1}
+                onEndReached={({distanceFromEnd}) => handleFetchMore(distanceFromEnd)}
+                ListFooterComponent={
+                   loadingMore ?  <ActivityIndicator color={colors.green} /> : <></>
+                }
             />
         </View>
     </SafeAreaView>
